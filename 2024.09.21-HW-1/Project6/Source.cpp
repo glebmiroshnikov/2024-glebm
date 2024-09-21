f8402d44a8099ef265484345b6ba3300b273efa9
ser Rogalik <st130023@student.spbu.ru>
2024-09-22 03:03:52 +0500

    HW1 6 tasks upload

--- 2024.09.21-HW-1/Project6/Source.cpp
+++ 2024.09.21-HW-1/Project6/Source.cpp
@@ -6,7 +6,7 @@ int main(int argv, char* argc[])
 	scanf_s("%d", &a);
 	int next = a + 1;
 	int prev = a - 1;
-	printf("The next number for %d is %d\n",a,next);
-	printf("The previous number for %d is %d",a,prev);
+	printf("The next number for the number %d is %d.\n",a,next);
+	printf("The previous number for the number %d is %d.\n",a,prev);
 	return EXIT_SUCCESS;
 }
